ec15c7079593b4fe9ee28cfbf209f5e81884e84f
Joost Lek <vlabakje@gmail.com>
2023-12-06 22:32:30 +0100

    parallel naive implementation for part two, works but is slow. estimate.py used for estimating how slow

---
+++ day05/estimate.py
@@ -0,0 +1,82 @@
+import multiprocessing as mp
+
+class Mapper():
+    def __init__(self, data):
+        seed_data, *block_data = list(data.split("\n\n"))
+        s = list(map(int, seed_data.split(": ")[1].split()))
+        self.seeds = [(s[i], s[i+1]) for i in range(0, len(s), 2)]
+        self.maps = {}
+        for b in block_data:
+            b = b.split("\n")
+            source, _, dest = b[0].split(" ")[0].partition("-to-")
+            self.maps[source] = Map(source, dest, b[1:])
+        # print(self.seeds)
+
+    def map(self, source, n):
+        return self.maps[source].map(n)
+
+    def final_locations_per_seed(self, n):
+        source = "seed"
+        # print(f"seed {n}")
+        while source != "location":
+            # print(f"{source=} {n=}")
+            source, n = self.map(source, n)
+        # print(f"result {n}")
+        return n
+
+    def min_for_range(self, q, start, length):
+        q.put(min(self.final_locations_per_seed(i) for i in range(start, start+length+1)))
+
+class Map():
+    def __init__(self, source, dest, data):
+        self.source = source
+        self.dest = dest
+        self._ranges = []
+        for d in data:
+            if d:
+                self._ranges.append(Range(*map(int, d.split())))
+
+    def map(self, n):
+        for r in self._ranges:
+            # print(f"{self.source=} {self.dest=} {r.source=} {r.dest=} {r.length=}")
+            if n in r:
+                # print("match")
+                return self.dest, r.map(n)
+        return self.dest, n
+            
+
+class Range():
+    def __init__(self, dest, source, length):
+        self.dest = dest
+        self.source = source
+        self.length = length
+    
+    def __contains__(self, n):
+        return self.source <= n < self.source+self.length
+
+    def map(self, n):
+        assert n in self
+        offset = n - self.source
+        return offset + self.dest
+
+def main(filename):
+    with open(filename) as fileh:
+        mapper = Mapper(fileh.read())
+        q = mp.Queue()
+        processes = []
+        maxlen = 0
+        for start, length in mapper.seeds:
+            maxlen = max(maxlen, length)
+            length = 1000000
+            processes.append(mp.Process(target=mapper.min_for_range, args=(q, start, length)))
+            processes[-1].start()
+        print(maxlen)
+        [p.join() for p in processes]
+        results = []
+        while not q.empty():
+            results.append(q.get())
+        return min(results)
+
+if __name__ == "__main__":
+    print(main("example"))
+    print(main("input"))
--- day05/two.py
+++ day05/two.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 class Mapper():
     def __init__(self, data):
         seed_data, *block_data = list(data.split("\n\n"))
-        self.seed = Series(list(map(int, seed_data.split(": ")[1].split())))
-        print(self.seed)
+        self.series = Series(list(map(int, seed_data.split(": ")[1].split())))
+        print(self.series)
         self.maps = {}
         for b in block_data:
             b = b.split("\n")
@@ -14,10 +14,11 @@ class Mapper():
     def final_locations_per_seed(self):
         source = "seed"
         while source != "location":
-            source = self.maps[source].map(self.seed)
-            #return
-        yield self.seed.min()
-        print(f"minimum {self.seed.min()=}")
+            print(f"[map] {source} {self.maps[source]}")
+            source = self.maps[source].map(self.series)
+            #if source == "light": return
+        yield self.series.min()
+        print(f"minimum {self.series.min()=}")
 
 class Map():
     def __init__(self, source, dest, data):
@@ -30,6 +31,36 @@ class Map():
 
     def map(self, series):
         print(f"[mapping] s={self.source} d={self.dest} {series=}")
+        _news = dict()
+        source = dict(series._series.items())
+        #for s, e in sorted(series._series.items()):
+        while len(source):
+            print(f"  {source=}")
+            s, e = min(source.keys()), source.pop(min(source.keys()))
+            #_news = {}
+            found = False
+            for r in sorted(self._ranges, key=lambda r: r.source):
+                if overlap := r.overlap(s, e):
+                    print(f"  apply {overlap=} {s=} {e=} {r=} {_news=} {overlap[0]+r.offset()},{overlap[1]+r.offset()}")
+                    for a, b in r.remaining(s, e):
+                        print(f"    remainder: {a=}, {b=}")
+                        if b == e:
+                            source[a] = b
+                        else:
+                            _news = add_series(_news, a, b)
+                    # assert overlap[0]+r.offset() not in _series
+                    #_series[overlap[0]+r.offset()] = overlap[1]+r.offset()
+                    _news = add_series(_news, overlap[0]+r.offset(), overlap[1]+r.offset())
+                    found = True
+                    break
+            if not found:
+                _news = add_series(_news, s, e)
+            #_s = _news
+            #print(f"[mapping] series {s=},{e=} completed {_s=} {r=}")
+        series._series = _news
+        print(f"[mapping] done s={self.source} d={self.dest} {series=}")
+        return self.dest
+        # old function
         for r in self._ranges:
             #print(f"mapping s={self.source} d={self.dest} rs={r.source} rd={r.dest} {r.length=} {series=}")
             if series.apply(r):
@@ -38,51 +69,8 @@ class Map():
         return self.dest
             
 
-class Series():
-    def __init__(self, seedpairs):
-        self._series = {seedpairs[i]: seedpairs[i]+seedpairs[i+1]-1 for i in range(0, len(seedpairs), 2)}
-
-    def apply(self, r):
-        _series = {}
-        changed = False
-        for s, e in sorted(self._series.items()):
-            if overlap := r.overlap(s, e):
-                for a, b in r.remaining(s, e):
-                    _series = add_series(_series, a, b)
-                print(f"apply {overlap=} {r=} {_series=} {overlap[0]+r.offset()},{overlap[1]+r.offset()}")
-                # assert overlap[0]+r.offset() not in _series
-                #_series[overlap[0]+r.offset()] = overlap[1]+r.offset()
-                _series = add_series(_series, overlap[0]+r.offset(), overlap[1]+r.offset())
-                changed = True
-            else:
-                _series = add_series(_series, s, e)
-        self._series = _series
-        return changed
-
     def __repr__(self):
-        return f"S({' '.join(str(k)+'-'+str(v) for k, v in sorted(self._series.items()))} "\
-                f"{sum(v-k for k, v in self._series.items())})"
-
-    def min(self):
-        return min(self._series.keys())
-
-
-
-def add_series(series, start, end):
-    out ={}
-    merged = False
-    for s, e in series.items():
-        o_s = max(start, s)
-        o_e = min(end, e)
-        if o_s <= o_e:
-            out[min(start, s)] = max(end, e)
-            merged = True
-        else:
-            out[s] = e
-    if not merged:
-        out[start] = end
-    return out
-
+        return f"Map({', '.join(str(r) for r in self._ranges)})"
 
 @dataclass
 class Range():
@@ -92,7 +80,7 @@ class Range():
     
     def overlap(self, s, e):
         o_s = max(self.source, s)
-        o_e = min(self.source + self.length, e)
+        o_e = min(self.source + self.length-1, e)
         if o_s <= o_e:
             return o_s, o_e
     
@@ -100,11 +88,11 @@ class Range():
         out = []
         o = self.overlap(s, e)
         if s < o[0]:
-            print(f"[remaining] {self=} {o=} {s=} {e=} {o[0]-1=}")
+            #print(f"[remaining] {self=} {o=} {s=} {e=} {o[0]-1=}")
             out.append((s, o[0]-1))
         if e > o[1]:
-            print(f"[remaining2] {self=} {o=} {s=} {e=} {o[0]=}")
-            out.append((o[1], e))
+            #print(f"[remaining2] {self=} {o=} {s=} {e=} {o[0]=}")
+            out.append((o[1]+1, e))
         return out
 
     def offset(self):
@@ -112,6 +100,40 @@ class Range():
 
     def __repr__(self):
         return f"Range(s{self.source}-{self.source+self.length-1} d{self.dest}-{self.dest+self.length-1} o={self.dest-self.source})"
+
+class Series():
+    def __init__(self, seedpairs):
+        self._series = {seedpairs[i]: seedpairs[i]+seedpairs[i+1]-1 for i in range(0, len(seedpairs), 2)}
+
+    def __repr__(self):
+        return f"S({' '.join(str(k)+'-'+str(v) for k, v in sorted(self._series.items()))} "\
+                f"{sum(v-k+1 for k, v in self._series.items())})"
+
+    def min(self):
+        return min(self._series.keys())
+
+
+def add_series(series, start, end):
+    print(f"    [add_series] {series=} {start=} {end=}")
+    out ={}
+    merged = False
+    for s, e in series.items():
+        o_s = max(start, s)
+        o_e = min(end, e)
+        if o_s <= o_e:
+            out[min(start, s)] = max(end, e)
+            merged = True
+        elif end + 1 == s:
+            out[start] = e
+            merged = True
+        elif e + 1 == start:
+            out[s] = end
+            merged = True
+        else:
+            out[s] = e
+    if not merged:
+        out[start] = end
+    return out
 
 
 def main(filename):
@@ -122,6 +144,6 @@ def main(filename):
 if __name__ == "__main__":
     print("="*20)
     print(main("example"))
-    #r = main("input")
+    r = main("input")
     print(r)
     assert r != 530455385
