b2c47a1cdd121c1dcc9c4d9f6c8609be1b42c979
Joost Lek <vlabakje@gmail.com>
2023-12-07 07:06:25 +0100

    day 07 complete

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Working on [Advent of Code 2023](https://adventofcode.com/2023/)!
 
-![](https://img.shields.io/badge/stars%20⭐-12-yellow) ![](https://img.shields.io/badge/days%20completed-6-red)
+![](https://img.shields.io/badge/stars%20⭐-14-yellow) ![](https://img.shields.io/badge/days%20completed-7-red)
 
 | day | Problem             | Part one                 | Part two                 | Stars won                                            |
 |----:|:--------------------|:-------------------------|:-------------------------|:-----------------------------------------------------|
@@ -12,6 +12,7 @@ Working on [Advent of Code 2023](https://adventofcode.com/2023/)!
 |  04 | Scratchcards        | [**one**](day04/one.py) | [**two**](day04/two.py) | ![](https://img.shields.io/badge/stars%20⭐-2-yellow) |
 |  05 | If you give a seed  | [**one**](day05/one.py) | [**two**](day05/two.py) | ![](https://img.shields.io/badge/stars%20⭐-2-yellow) |
 |  06 | Wait for it         | [**one**](day06/one.py) | [**two**](day06/two.py) | ![](https://img.shields.io/badge/stars%20⭐-2-yellow) |
+|  07 | Camel Cards         | [**one**](day07/one.py) | [**two**](day07/two.py) | ![](https://img.shields.io/badge/stars%20⭐-2-yellow) |
 
 *Badges from: https://github.com/J0B10/aoc-badges-action*
 
---
+++ day07/one.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from collections import Counter
+from enum import IntEnum
+
+class Types(IntEnum):
+    FIVE = 1
+    FOUR = 2
+    FULLHOUSE = 3
+    THREE = 4
+    TWOPAIR = 5
+    ONEPAIR = 6
+    HIGH = 7
+    NONE = 8
+
+CARDVALUES={v: k for k,v in enumerate("AKQJT98765432")}
+
+@dataclass(order=True)
+class Hand:
+    cards: str
+    bid: int
+
+    def __post_init__(self):
+        ix = [self._type()] + [CARDVALUES[c] for c in self.cards]
+        self.sort_index = tuple(ix)
+
+    def _type(self):
+        c = Counter(self.cards)
+        s = tuple(sorted(c.values()))
+        # print(c, s)
+        if len(s) == 1:
+            return Types.FIVE
+        if s == (1, 4):
+            return Types.FOUR
+        if s == (2, 3):
+            return Types.FULLHOUSE
+        if s == (1, 1, 3):
+            return Types.THREE
+        if s == (1, 2, 2):
+            return Types.TWOPAIR
+        if s == (1, 1, 1, 2):
+            return Types.ONEPAIR
+        if s == (1, 1, 1, 1, 1):
+            return Types.HIGH
+        return Types.NONE
+
+def main(filename):
+    with open(filename) as fileh:
+        hands = []
+        for line in fileh:
+            cards, _, bid = line.partition(" ")
+            hands.append(Hand(cards, int(bid)))
+        hands = sorted(hands, key=lambda h: h.sort_index, reverse=True)
+        #for rank, h in enumerate(hands, start=1):
+        #    print(rank, h, h.sort_index)
+        return sum(rank * h.bid for rank, h in enumerate(hands, start=1))
+
+if __name__ == "__main__":
+    print(main("example"))
+    print(main("input"))
